db2382e140d728018f0f02cfe84294cc3fd40116
Hi-nu <roughskethc1104@gmail.com>
2022-11-24 17:36:15 +0900

    start

---
+++ test_git/src/body.js
@@ -0,0 +1,9 @@
+export default function Body(){
+    return(
+        <Container>
+            <div>
+                <Button variant="primary">Test Button</Button>
+            </div>
+            </Container>
+    )
+}
